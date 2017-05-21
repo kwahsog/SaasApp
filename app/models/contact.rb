632be6cd4988b02ab03331eab62635b4ccfa8ae0
Alex <cooper.alexander@gmail.com>
2017-05-21 14:58:06 +0100

    Addec contact create action to save to db

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ActiveRecord::Base
-
+	#rails automatically checks the database to get class properties, so you do not need to explicility define it.
+	#e.g. name, email, comments are automatically passed in.
 end
