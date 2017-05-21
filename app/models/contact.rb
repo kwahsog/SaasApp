3cac8aff650972d41f3447588e6320be28f2fca8
Alex <cooper.alexander@gmail.com>
2017-05-21 18:07:30 +0100

    Added form validations to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,7 @@
 class Contact < ActiveRecord::Base
 	#rails automatically checks the database to get class properties, so you do not need to explicility define it.
 	#e.g. name, email, comments are automatically passed in.
+	validates :name, presence: true
+	validates :email, presence: true
+	validates :comments, presence: true
 end
